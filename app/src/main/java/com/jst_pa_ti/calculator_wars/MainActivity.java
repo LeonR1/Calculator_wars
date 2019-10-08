26b9dcbcf6523af13693eb211c94e1bc991d081f
Bostjan <funnycinema123@gmail.com>
2019-10-08 19:48:43 +0200

    random update

--- app/src/main/java/com/jst_pa_ti/calculator_wars/MainActivity.java
+++ app/src/main/java/com/jst_pa_ti/calculator_wars/MainActivity.java
@@ -3,11 +3,15 @@ package com.jst_pa_ti.calculator_wars;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int stRacunov = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +24,20 @@ public class MainActivity extends AppCompatActivity {
         TextView seedView = findViewById(R.id.textView);
         seedView.setText("" + seed);
 
-        RacunGenerator racunGenerator = new RacunGenerator(seed);
+        final RacunGenerator racunGenerator = new RacunGenerator(seed);
 
-        for (int i = 0; i < 100; i++) {
+        Button button = findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
 
-            System.out.println(racunGenerator.getNumber());
+            @Override
+            public void onClick(View v) {
 
-        }
+                TextView textView = findViewById(R.id.textView3);
+                textView.setText(racunGenerator.getRacun());
+
+            }
+
+        });
 
     }
 }
--- app/src/main/java/com/jst_pa_ti/calculator_wars/RacunGenerator.java
+++ app/src/main/java/com/jst_pa_ti/calculator_wars/RacunGenerator.java
@@ -7,6 +7,7 @@ public class RacunGenerator {
     private int difficulty;
     private int seed;
     private Random random;
+    private int rezultat;
 
     public RacunGenerator(int seed) {
 
@@ -16,54 +17,113 @@ public class RacunGenerator {
 
     }
 
+    public String getRacun() {
+
+        int a = getNumber(false);
+        int b = getNumber(false);
+        String s = getMathSymbol();
+        if (b == 0 && s.equals("/"))
+            b = getNumber(true);
+
+        setRezultat(a, b, s);
+
+        return a + s + b + " = " + rezultat;
+
+    }
+
+    private void setRezultat(int a, int b, String s) {
+
+        switch (s) {
+
+            case "+":
+                rezultat = a + b;
+                break;
+            case "-":
+                rezultat = a - b;
+                break;
+            case "*":
+                rezultat = a * b;
+                break;
+            case "/":
+                if (b == 0)
+                    b = getNumber(true);
+                rezultat = a / b;
+                break;
+
+        }
+
+    }
+
     private int getRacunType() {
 
         return random.nextInt(3);
 
     }
 
-    private char getMathSymbol() {
+    private String getMathSymbol() {
 
         int symbol = random.nextInt(4);
 
         switch (symbol) {
 
             case 0:
-                return '+';
+                return "+";
             case 1:
-                return '-';
+                return "-";
             case 2:
-                return '*';
+                return "*";
             case 3:
-                return '/';
+                return "/";
 
         }
 
-        return 0;
+        return null;
 
     }
 
-    public void setDifficulty(int stRacunov) {
+    private void setDifficulty(int stRacunov) {
 
         if (stRacunov % 5 == 0 && stRacunov != 0)
             difficulty++;
 
     }
 
-    public int getNumber() {
+    private int getNumber(boolean dz0) {
 
-        switch (difficulty) {
+        if (!dz0) {
 
-            case 0:
-                return random.nextInt(10);
-            case 1:
-                return random.nextInt(100);
-            case 2:
-                return random.nextInt(90) + 10;
-            case 3:
-                return random.nextInt(990) + 10;
-            default:
-                return random.nextInt(900) + 100;
+            switch (difficulty) {
+
+                case 0:
+                    return random.nextInt(10);
+                case 1:
+                    return random.nextInt(100);
+                case 2:
+                    return random.nextInt(90) + 10;
+                case 3:
+                    return random.nextInt(990) + 10;
+                default:
+                    return random.nextInt(900) + 100;
+
+            }
+
+        }
+        else {
+
+            switch (difficulty) {
+
+                case 0:
+                    return random.nextInt(9) + 1;
+                case 1:
+                    return random.nextInt(99) + 1;
+                case 2:
+                    return random.nextInt(89) + 11;
+                case 3:
+                    return random.nextInt(989) + 11;
+                default:
+                    return random.nextInt(899) + 101;
+
+            }
 
         }
 
