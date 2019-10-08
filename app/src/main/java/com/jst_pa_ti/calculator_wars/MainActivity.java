038b5a577b4bbd5fd01b0ba3323b5a1eab00a338
Leon <leon.rijavec1@gmail.com>
2019-10-08 08:50:34 +0200

    Initial commit

---
+++ app/src/main/java/com/jst_pa_ti/calculator_wars/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jst_pa_ti.calculator_wars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
